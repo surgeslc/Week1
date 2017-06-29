4bfb8bd83e83b95311f22f2f64c912c81cb62900
Lawrence Surges <surgeslc@gmail.com>
2017-06-29 03:59:27 +0000

    Removed console.log calls :q

--- CharacterPositioning/letterPositions.js
+++ CharacterPositioning/letterPositions.js
@@ -17,14 +17,14 @@ for (i = 0; i <= string.length - 1; i++) {
     continue; //Next i
   }
   if (outputReport.hasOwnProperty(currentLetter) == false) {
-      console.log("Key", currentLetter, "doesn't exist");
-      console.log("Must add the key and value");
+      //console.log("Key", currentLetter, "doesn't exist");
+      //console.log("Must add the key and value");
       outputReport[currentLetter] = [i];
-      console.log("outputReport at", i, "is", outputReport);
+      //console.log("outputReport at", i, "is", outputReport);
     //outputReport[currentLetter] = i;    //console.log(Object.keys(outputReport));
       } else {
-        console.log("Key", currentLetter, "exists");
-        console.log("Must push the current position to the array");
+        //console.log("Key", currentLetter, "exists");
+        //console.log("Must push the current position to the array");
         outputReport[currentLetter].push(i);
     //Object.currentLetter.
     //outputReport[currentLetter].push(i);
